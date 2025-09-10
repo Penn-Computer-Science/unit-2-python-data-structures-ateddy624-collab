d12341fdb3b2a665f95ede898288e31678d5d66a
ateddy624-collab <ateddy624@phm.k12.in.us>
2025-09-10 09:49:18 -0400

    Update DataReview.py

--- DataReview.py
+++ DataReview.py
@@ -1,10 +1,48 @@
-practice_list=[1,154,235,31,6,32,23,893,94,10]
-print("The first number is "+str(practice_list[0]))
-print("The last number is "+str(practice_list[9]))
-print("The middle number is "+str(practice_list[5]))
-practice_list.append(846)
-practice_list[2]=99
-print("The even numbers are: ")
-for i in practice_list:
-    if i%2==0:
-        print(i)
+#initializes the list
+#practice_list=[1,154,235,31,6,32,23,893,94,10]
+#prints the first, last, and middle(ish) numbers in the list
+#print("The first number is "+str(practice_list[0]))
+#print("The last number is "+str(practice_list[9]))
+#print("The middle number is "+str(practice_list[5]))
+#changes the list
+#practice_list.append(846)
+#practice_list[2]=99
+#announces and prints only the even numbers
+#print("The even numbers are: ")
+#for i in practice_list:
+#    if i%2==0:
+#        print(i)
+
+
+#initializes the dictionary
+#practice_dict={"United States":"Washington DC", "Canada":"Ottawa", "China":"Beijing","Russia":"Moscow", "Poland":"Warsaw"}
+#prints two countries' capitals
+#print(practice_dict["United States"])
+#print(practice_dict["Canada"])
+#adds to the dictionary
+#practice_dict["Ukraine"]="Kyiv"
+#changes the capital of Canada
+#practice_dict["Canada"]="Washington DC"
+#prints all of the new countries and capitals
+#for i in (practice_dict):
+#    print(i+": "+str(practice_dict[i]))
+
+
+##initializes the practice set
+#practice_set={"apples","bananas","grapes","cherries","strawberries","raspberries"}
+##adds "pinapples" to the practice set
+#practice_set.add("pineapples")
+##initializes friend's practice set
+#friend_practice_set={"kiwis","mangos","bananas","pineapples","oranges"}
+##creates and prints a set with the intersection of our sets
+#inter_set=practice_set.intersection(friend_practice_set)
+#print(inter_set)
+##creates and prints a set with the difference of our sets
+#solo_yo=practice_set - friend_practice_set
+#print(solo_yo)
+##creates and prints a set with the union of our sets
+#todos_fruits=practice_set.union(friend_practice_set)
+#print(todos_fruits)
+
+
+#initializes my practice tuple
