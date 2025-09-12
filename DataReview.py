967b88126d8f110367b81084d5ee7f353805d5b3
ateddy624-collab <ateddy624@phm.k12.in.us>
2025-09-12 09:53:52 -0400

    a
    ....

--- DataReview.py
+++ DataReview.py
@@ -1,48 +1,56 @@
 #initializes the list
-#practice_list=[1,154,235,31,6,32,23,893,94,10]
+practice_list=[1,154,235,31,6,32,23,893,94,10]
 #prints the first, last, and middle(ish) numbers in the list
-#print("The first number is "+str(practice_list[0]))
-#print("The last number is "+str(practice_list[9]))
-#print("The middle number is "+str(practice_list[5]))
+print("The first number is "+str(practice_list[0]))
+print("The last number is "+str(practice_list[9]))
+print("The middle number is "+str(practice_list[5]))
 #changes the list
-#practice_list.append(846)
-#practice_list[2]=99
+practice_list.append(846)
+practice_list[2]=99
 #announces and prints only the even numbers
-#print("The even numbers are: ")
-#for i in practice_list:
-#    if i%2==0:
-#        print(i)
-
+print("The even numbers are: ")
+for i in practice_list:
+    if i%2==0:
+        print(i)
 
 #initializes the dictionary
-#practice_dict={"United States":"Washington DC", "Canada":"Ottawa", "China":"Beijing","Russia":"Moscow", "Poland":"Warsaw"}
+practice_dict={"United States":"Washington DC", "Canada":"Ottawa", "China":"Beijing","Russia":"Moscow", "Poland":"Warsaw"}
 #prints two countries' capitals
-#print(practice_dict["United States"])
-#print(practice_dict["Canada"])
+print(practice_dict["United States"])
+print(practice_dict["Canada"])
 #adds to the dictionary
-#practice_dict["Ukraine"]="Kyiv"
+practice_dict["Ukraine"]="Kyiv"
 #changes the capital of Canada
-#practice_dict["Canada"]="Washington DC"
+practice_dict["Canada"]="Washington DC"
 #prints all of the new countries and capitals
-#for i in (practice_dict):
-#    print(i+": "+str(practice_dict[i]))
+for i in (practice_dict):
+    print(i+": "+str(practice_dict[i]))
 
 
-##initializes the practice set
-#practice_set={"apples","bananas","grapes","cherries","strawberries","raspberries"}
-##adds "pinapples" to the practice set
-#practice_set.add("pineapples")
-##initializes friend's practice set
-#friend_practice_set={"kiwis","mangos","bananas","pineapples","oranges"}
-##creates and prints a set with the intersection of our sets
-#inter_set=practice_set.intersection(friend_practice_set)
-#print(inter_set)
-##creates and prints a set with the difference of our sets
-#solo_yo=practice_set - friend_practice_set
-#print(solo_yo)
-##creates and prints a set with the union of our sets
-#todos_fruits=practice_set.union(friend_practice_set)
-#print(todos_fruits)
+#initializes the practice set
+practice_set={"apples","bananas","grapes","cherries","strawberries","raspberries"}
+#adds "pinapples" to the practice set
+practice_set.add("pineapples")
+#initializes friend's practice set
+friend_practice_set={"kiwis","mangos","bananas","pineapples","oranges"}
+#creates and prints a set with the intersection of our sets
+inter_set=practice_set.intersection(friend_practice_set)
+print(inter_set)
+#creates and prints a set with the difference of our sets
+solo_yo=practice_set - friend_practice_set
+print(solo_yo)
+#creates and prints a set with the union of our sets
+todos_fruits=practice_set.union(friend_practice_set)
+print(todos_fruits)
 
 
 #initializes my practice tuple
+practice_tuple=("red", "orange", "blue", "purple", "pink")
+#prints the first and last color in the tuple
+print(practice_tuple[0])
+print(practice_tuple[4])
+#prints all of the colors in the tuple
+for i in practice_tuple:
+    print(i)
+practice_tuple[0]="rojo"
+#the error says "TypeError: 'tuple' object does not support item assignment"
--- gradebookManager.py
+++ gradebookManager.py
@@ -1,2 +1,25 @@
-
-#change
+a_grade=(90)
+b_grade=(80)
+c_grade=(70)
+d_grade=(60)
+class_dict={"Andrew":[91, 97, 88, 75, 98, 95] ,"Alice":[89, 99, 96, 100, 97, 98] ,"Anthony":[98, 87, 68, 75, 87, 86]}
+def print_grades(person):
+    try:
+        print(person+"'s grades are: "+str(class_dict[person]))
+    except KeyError:
+        print("Please try again")
+def calc_avg_grade(person):
+    try:
+        person_avg=0
+        number=0
+        for i in class_dict[person]:
+            person_avg+=i
+            number+=1
+        person_avg=person_avg/number
+        person_avg=round(person_avg, 2)
+        print(person+"'s average grade is "+str(person_avg)+"%")
+    except KeyError:
+        print("Invalid student. Please try again")
+def
+#print_grades(input("Select a student to view their grades "))
+#calc_avg_grade(input("Select a student to view their average grade "))
