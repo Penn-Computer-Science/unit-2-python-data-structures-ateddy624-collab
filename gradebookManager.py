a2d59863dca3521ed8f496038ec373d444ffcbc7
ateddy624-collab <ateddy624@phm.k12.in.us>
2025-09-15 21:19:24 -0400

    .

--- gradebookManager.py
+++ gradebookManager.py
@@ -3,6 +3,7 @@ b_grade=(80)
 c_grade=(70)
 d_grade=(60)
 class_dict={"Andrew":[91, 97, 88, 75, 98, 95] ,"Alice":[89, 99, 96, 100, 97, 98] ,"Anthony":[98, 87, 68, 75, 87, 86]}
+
 def print_grades(person):
     try:
         print(person+"'s grades are: "+str(class_dict[person]))
@@ -20,6 +21,36 @@ def calc_avg_grade(person):
         print(person+"'s average grade is "+str(person_avg)+"%")
     except KeyError:
         print("Invalid student. Please try again")
-def
+def add_student(person):
+    class_dict[person]=[]
+def grade_summary():
+    print(class_dict)
+def high_grade():
+    high_grade=0
+    who_high_grade=[]
+    for i in class_dict:
+        high_percent=calc_avg_grade(i)
+        if high_percent>high_grade:
+            while True:
+                try:
+                    who_high_grade.pop()
+                except IndexError:
+                    break
+            high_grade=high_percent
+            who_high_grade[0]=i
+        elif high_percent==high_grade:
+            num=1
+            for i in who_high_grade:
+                num+=1
+            who_high_grade[num]=i
+    return highest_grade="The student with the highest average score is "+str(who_high_grade)+" with a score of "+str(high_grade)
+
+
+
+print(high_grade())
+#add_student(input("Enter student name: "))
 #print_grades(input("Select a student to view their grades "))
 #calc_avg_grade(input("Select a student to view their average grade "))
+
+
+#while True:
